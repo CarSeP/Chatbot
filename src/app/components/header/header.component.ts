254984f5f74f4fb1f2d7adb2d0886ad7e440a861
CarSeP <vjpd64@gmail.com>
2024-03-25 09:45:35 -0400

    add dark mode

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styles: ``
 })
 export class HeaderComponent {
+  darkMode = signal(false)
 
+  toggleTheme() {
+    this.darkMode.set(!this.darkMode())
+
+    if (this.darkMode()) {
+      document.body.classList.add("dark")
+    } else {
+      document.body.classList.remove("dark")
+    }
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,16 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        darkBackground: '#141414',
+        inputDark: '#2c2c2c',
+        buttonDark: '#443dc7',
+        buttonDarkHover: '#5b4cff'
+      }
+    },
   },
   plugins: [],
 }
